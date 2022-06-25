eb80e79079a98090322385922c4efe8a0dfc3ee3
jpchionglo <jpchionglo@gmail.com>
2022-06-24 22:25:41 -0500

    catching duplicate name early for addcard

--- src/commands/addcard.ts
+++ src/commands/addcard.ts
@@ -25,102 +25,116 @@ export const addcard: Command = {
   run: async (client: Client, interaction: BaseCommandInteraction) => {
     const name = interaction.options.get("name")?.value;
 
-    const modal = new Modal()
-      .setCustomId("addPhotoCard")
-      .setTitle(`Add a Photocard (${name})`);
-
-    const imageUrl = new TextInputComponent()
-      .setCustomId("imageUrlInput")
-      .setLabel("Image URL")
-      .setStyle("SHORT")
-      .setRequired(true);
-
-    const rarity = new TextInputComponent()
-      .setCustomId("rarityInput")
-      .setLabel("Rarity")
-      .setStyle("SHORT")
-      .setRequired(true);
-
-    const era = new TextInputComponent()
-      .setCustomId("eraInput")
-      .setLabel("Era")
-      .setStyle("SHORT")
-      .setRequired(true);
-
-    const set = new TextInputComponent()
-      .setCustomId("setInput")
-      .setLabel("Set #")
-      .setStyle("SHORT")
-      .setRequired(true);
-
-    const cost = new TextInputComponent()
-      .setCustomId("costInput")
-      .setLabel("Cost")
-      .setStyle("SHORT")
-      .setRequired(true);
-
-    const firstActionRow =
-      new MessageActionRow<ModalActionRowComponent>().addComponents(imageUrl);
-
-    const secondActionRow =
-      new MessageActionRow<ModalActionRowComponent>().addComponents(rarity);
-
-    const thirdActionRow =
-      new MessageActionRow<ModalActionRowComponent>().addComponents(era);
-
-    const fourthActionRow =
-      new MessageActionRow<ModalActionRowComponent>().addComponents(set);
-
-    const fifthActionRow =
-      new MessageActionRow<ModalActionRowComponent>().addComponents(cost);
-
-    modal.addComponents(
-      firstActionRow,
-      secondActionRow,
-      thirdActionRow,
-      fourthActionRow,
-      fifthActionRow
-    );
-
-    await interaction.showModal(modal);
-
-    client.once("interactionCreate", async (modalInteraction: Interaction) => {
-      if (
-        modalInteraction.isModalSubmit() &&
-        modalInteraction.customId === "addPhotoCard"
-      ) {
-        let modalImageUrl =
-          modalInteraction.fields.getTextInputValue("imageUrlInput");
-        let modalRarity =
-          modalInteraction.fields.getTextInputValue("rarityInput");
-        let modalEra = modalInteraction.fields.getTextInputValue("eraInput");
-        let modalSet = modalInteraction.fields.getTextInputValue("setInput");
-        let modalCost = modalInteraction.fields.getTextInputValue("costInput");
-        try {
-          const photocard = await Photocards.create(
-            {
-              name,
-              imageUrl: modalImageUrl,
-              rarity: modalRarity,
-              era: modalEra,
-              set: modalSet,
-              cost: modalCost,
-            },
-            { where: { name } }
-          );
-
-          return modalInteraction.reply(`Card ${photocard.name} added!`);
-        } catch (error: any) {
-          console.log(error);
-          if (error.name === "SequelizeUniqueConstraintError") {
-            return modalInteraction.reply("That photocard already exists.");
-          }
-
-          return modalInteraction.reply(
-            "Something went wrong with adding a photocard."
-          );
-        }
-      }
+    const photocard = await Photocards.findOne({
+      where: { name },
     });
+
+    if (photocard) {
+      interaction.reply("A photocard with that name already exists.");
+    } else {
+      const modal = new Modal()
+        .setCustomId("addPhotoCard")
+        .setTitle(`Add a Photocard (${name})`);
+
+      const imageUrl = new TextInputComponent()
+        .setCustomId("imageUrlInput")
+        .setLabel("Image URL")
+        .setStyle("SHORT")
+        .setRequired(true);
+
+      const rarity = new TextInputComponent()
+        .setCustomId("rarityInput")
+        .setLabel("Rarity")
+        .setStyle("SHORT")
+        .setRequired(true);
+
+      const era = new TextInputComponent()
+        .setCustomId("eraInput")
+        .setLabel("Era")
+        .setStyle("SHORT")
+        .setRequired(true);
+
+      const set = new TextInputComponent()
+        .setCustomId("setInput")
+        .setLabel("Set #")
+        .setStyle("SHORT")
+        .setRequired(true);
+
+      const cost = new TextInputComponent()
+        .setCustomId("costInput")
+        .setLabel("Cost")
+        .setStyle("SHORT")
+        .setRequired(true);
+
+      const firstActionRow =
+        new MessageActionRow<ModalActionRowComponent>().addComponents(imageUrl);
+
+      const secondActionRow =
+        new MessageActionRow<ModalActionRowComponent>().addComponents(rarity);
+
+      const thirdActionRow =
+        new MessageActionRow<ModalActionRowComponent>().addComponents(era);
+
+      const fourthActionRow =
+        new MessageActionRow<ModalActionRowComponent>().addComponents(set);
+
+      const fifthActionRow =
+        new MessageActionRow<ModalActionRowComponent>().addComponents(cost);
+
+      modal.addComponents(
+        firstActionRow,
+        secondActionRow,
+        thirdActionRow,
+        fourthActionRow,
+        fifthActionRow
+      );
+
+      await interaction.showModal(modal);
+
+      client.once(
+        "interactionCreate",
+        async (modalInteraction: Interaction) => {
+          if (
+            modalInteraction.isModalSubmit() &&
+            modalInteraction.customId === "addPhotoCard"
+          ) {
+            let modalImageUrl =
+              modalInteraction.fields.getTextInputValue("imageUrlInput");
+            let modalRarity =
+              modalInteraction.fields.getTextInputValue("rarityInput");
+            let modalEra =
+              modalInteraction.fields.getTextInputValue("eraInput");
+            let modalSet =
+              modalInteraction.fields.getTextInputValue("setInput");
+            let modalCost =
+              modalInteraction.fields.getTextInputValue("costInput");
+            try {
+              const photocard = await Photocards.create(
+                {
+                  name,
+                  imageUrl: modalImageUrl,
+                  rarity: modalRarity,
+                  era: modalEra,
+                  set: modalSet,
+                  cost: modalCost,
+                },
+                { where: { name } }
+              );
+
+              return modalInteraction.reply(`Card ${photocard.name} added!`);
+            } catch (error: any) {
+              console.log(error);
+              if (error.name === "SequelizeUniqueConstraintError") {
+                return modalInteraction.reply("That photocard already exists.");
+              }
+
+              return modalInteraction.reply(
+                "Something went wrong with adding a photocard."
+              );
+            }
+          }
+        }
+      );
+    }
   },
 };
